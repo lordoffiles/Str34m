5c41c2803bfd5ce617f83a1de3665647836d4d60
Charlie Beaulieu <excel1vmn@gmail.com>
2022-02-13 18:57:42 -0500

    added new side scene and tweaked caracter mouvement

--- unity/Assets/Code/player_controller.cs
+++ unity/Assets/Code/player_controller.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class player_controller : MonoBehaviour
 {
     [SerializeField]
-    float moveSpeed = 1f;
+    float moveSpeed = 10f;
 
     [SerializeField]
-    float jumpImpulse = 1f;
+    float jumpAmount = 15f;
+
+    [SerializeField]
+    float jumpHeight = 5f;
+
+    [SerializeField]
+    float gravityScale = 3.5f;
+
+    [SerializeField]
+    float fallingGravityScale = 5f;
 
     [SerializeField]
     Rigidbody2D rb;
 
     private bool canJetpack = false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -25,26 +35,42 @@ public class player_controller : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
 
-        rb.AddForce(Vector3.right * horizontalInput * moveSpeed/100);
+        rb.AddForce(Vector2.right * horizontalInput * moveSpeed);
 
-        if (canJetpack == false &&  Input.GetAxis("Jump") > 0)
+        // if (canJetpack == false &&  Input.GetAxis("Jump") > 0)
+        // {
+        //     canJetpack = true;
+        // }
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            canJetpack = true;
+            rb.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
+            // rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            
+        }
+        if (rb.velocity.y >= 0)
+        {
+            rb.gravityScale = gravityScale;
+        }
+        else if (rb.velocity.y < 0)
+        {
+            rb.gravityScale = fallingGravityScale;
         }
 
     }
 
     void resetCanJump()
     {
-        canJetpack = true;
+        // canJetpack = true;
     }
 
     private void FixedUpdate()
     {
-        if(canJetpack)
-        {
-            rb.AddForce(new Vector2(0f,jumpImpulse/100), ForceMode2D.Impulse);
-            canJetpack = false;
-        }
+        // if(canJetpack)
+        // {
+        //     // new Vector2(0f,jumpAmount)
+        //     rb.AddForce(Vector2.up * (jumpAmount / 20), ForceMode2D.Impulse);
+        //     canJetpack = false;
+        // }
     }
 }
